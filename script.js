af64953a8d9438bd5d0d09f77ce4f1c23d039b60
dennis027 <dennis.kimani@stdent.moringaschool.com>
2021-04-16 22:03:46 +0300

    add a toggle function

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
     $(".btn").click(function(){
-        $(".what p").toggle();
+        $(".design p").toggle();
         $(".btn").hide();
        
     });
-    $(".what p").click(function(){
+    $(".design p").click(function(){
         $(".btn").toggle();
-        $(".what p").hide();
+        $(".design p").hide();
+
+    });
+
+
+    $(".btn2").click(function(){
+        $(".development p").toggle();
+        $(".btn2").hide();
+       
+    });
+    $(".development p").click(function(){
+        $(".btn2").toggle();
+        $(".development p").hide();
 
     })
+
 
 });
